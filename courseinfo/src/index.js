ee0bfa580ebb9c9345bee6dd0ba6a6d904468cb3
marju <marjuit@hotmail.com>
2019-10-18 09:57:42 +0300

    valmis kood uuesti sisse

--- courseinfo/src/index.js
+++ courseinfo/src/index.js
@@ -1,12 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+const Header = (props) => {
+    return (
+        <h1>{props.course}</h1>
+    )
+} 
+
+
+
+const Content = ({ parts }) => {
+    return (
+        <div>
+            <Part part={parts[0]} />
+            <Part part={parts[1]} />
+            <Part part={parts[2]} />
+        </div>
+
+    )
+}
+
+const Part = (props) => {
+    return (
+        <div>
+            <p>
+                {props.part.name} {props.part.exercises}
+            </p>
+        </div>
+    )
+}
+
+const Total = (props) => {
+    return (
+        <div>
+            <p>
+                Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+            </p>
+        </div>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7 
+            },
+            {
+                name: 'State of component',
+                exercises: 14
+            }
+        ]
+    }
+    return (
+        <div>
+            <Header course={course.name} />
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
