f025ed347ea54ce7bcc87b31fe9309e81eb9df0f
marju <marjuit@hotmail.com>
2019-10-21 22:46:00 +0300

    anecdote toimib

--- anecdote/src/index.js
+++ anecdote/src/index.js
@@ -7,27 +7,65 @@ const Button = ({onClick, text}) => (
     </button>
 )
 
+const Header = ({text}) => (
+  <h2>{text}</h2>
+)
+
 const App = (props) => {
-  const [selected, setSelected] = useState(0)
+  const [selected, setSelected] = useState(5)
   
   const length = props.anecdotes.length
 
+  const [voted, setVoted] = useState(new Array(length).fill(0))
+
+  function getMax (array) {
+    const maxVotes = Math.max.apply(null, array)
+    console.log(Math.max.apply(null, array))
+    return maxVotes
+  }
+
+  const Max = getMax(voted)
+
+  function isMax (number) {
+    return number >= Max
+  }
+
+  //teha nii, et 
   function getRandomInt (number) {
       return Math.floor(Math.random() * Math.floor(number) )
   }
 
 
   const handleNextClick = () => {
-        setSelected(getRandomInt(length))
+    console.log('enne' + selected)    
+    setSelected(getRandomInt(length))
+    console.log('parast' + selected)
+  }
+
+  const handleVoteClick = () => {
+    console.log("selected " + selected)
+    const copyVotes = [...voted]
+    copyVotes[selected] += 1
+    setVoted(copyVotes)
+    console.log("voted " + voted)
+    console.log("copyVotes " + copyVotes)
   }
 
   return (
     <div>
+      <Header text="Anecdote of the day"/>
       <div>
         {props.anecdotes[selected]}
+        <p>has {voted[selected]} votes</p>
       </div>
-      <Button text="vote" /> 
+      <Button text="vote" onClick={handleVoteClick}/> 
       <Button text="next anecdote" onClick={handleNextClick}/>
+      <Header text="Anecdote with most votes" />
+      <div>
+        {props.anecdotes[voted.findIndex(isMax)]}
+        <p>has {voted[voted.findIndex(isMax)]} votes</p>
+      </div>
+
     </div>
   )
 }
